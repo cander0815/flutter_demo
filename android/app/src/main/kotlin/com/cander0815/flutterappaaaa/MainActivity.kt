f90cc12dda598f9597a22f167f916bc7b1286f58
cander0815 <cander0815@qq.com>
2020-05-13 15:31:22 +0800

    代码提交

---
+++ android/app/src/main/kotlin/com/cander0815/flutterappaaaa/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cander0815.flutterappaaaa
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
